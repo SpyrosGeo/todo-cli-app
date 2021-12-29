4122b718f2b9822a9c37dc663488d2dca234cbeb
SpyrosGeo <xarodasimba@gmail.com>
2021-12-29 03:05:46 +0200

    rewrote some code removed some files because of a bux will add them again later

---
+++ rptodo/__init__.py
@@ -0,0 +1,8 @@
+"""
+Entry point for the package
+"""
+#rptodo/__init__.py
+
+
+__app_name__ ="Todo_app"
+__version__ = "1.0.0"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,26 +1,23 @@
-"""
-Top-level package for RP To-DO
-"""
+"""Top-level package for RP To-DO"""
 #rptodo/__init__.py
 
-__app_name__ = "todo-cli"
-__version___ = "0.1.0"
+__app_name__ = "todo-app"
+__version__ = "0.1.0"
+# (
+#     SUCCESS,
+#     DIR_ERROR,
+#     FILE_ERROR,
+#     DB_READ_ERROR,
+#     DB_WRITE_ERROR,
+#     JSON_ERROR,
+#     ID_ERROR,
+# )= range(7)
 
-(
-    SUCCESS,
-    DIR_ERROR,
-    FILE_ERROR,
-    DB_READ_ERROR,
-    DB_WRITE_ERROR,
-    JSON_ERROR,
-    ID_ERROR,
-)= range(7)
 
-
-ERRORS = {
-    DIR_ERROR:"config directory error",
-    FILE_ERROR:"config file error",
-    DB_READ_ERROR:"database read error",
-    DB_WRITE_ERROR:"database write error",
-    ID_ERROR:"to-do id error"
-}
+# ERRORS = {
+#     DIR_ERROR:"config directory error",
+#     FILE_ERROR:"config file error",
+#     DB_READ_ERROR:"database read error",
+#     DB_WRITE_ERROR:"database write error",
+#     ID_ERROR:"to-do id error"
+# }
